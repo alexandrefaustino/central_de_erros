fa8679ed554f5b52311575863e0d1d9800939054
alexandrefaustino <alexandrefaculdade@gmail.com>
2021-04-04 15:51:48 -0300

    configuração inicial spring boot

---
+++ src/test/java/br/com/central_de_erros/CentralDeErrosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.central_de_erros;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CentralDeErrosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
