f7f4d39d6175b14b701d9ae78eafc816f2559288
EmanuelleBrasil <emanuelle.rbl@gmail.com>
2021-04-07 10:37:16 -0300

    Fazendo paginação no repository

--- src/main/java/br/com/central_de_erros/repository/LogErrosRepository.java
+++ src/main/java/br/com/central_de_erros/repository/LogErrosRepository.java
@@ -3,6 +3,7 @@ package br.com.central_de_erros.repository;
 import br.com.central_de_erros.model.LevelError;
 import br.com.central_de_erros.model.LogErros;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -10,13 +11,15 @@ import java.time.LocalDateTime;
 
 @Repository
 public interface LogErrosRepository extends JpaRepository<LogErros, Long> {
-    Page<LogErros> findByLevel(LevelError level);
+    Page<LogErros> findAll(Pageable pageable);
 
-    Page<LogErros> findByDescription(String description);
+    Page<LogErros> findByLevel(LevelError level, Pageable pageable);
 
-    Page<LogErros> findByOrigin(String origin);
+    Page<LogErros> findByDescription(String description, Pageable pageable);
 
-    Page<LogErros> findByDetails(String details);
+    Page<LogErros> findByOrigin(String origin, Pageable pageable);
 
-    Page<LogErros> findByDate(LocalDateTime date);
+    Page<LogErros> findByDetails(String details, Pageable pageable);
+
+    Page<LogErros> findByDate(LocalDateTime date, Pageable pageable);
 }
