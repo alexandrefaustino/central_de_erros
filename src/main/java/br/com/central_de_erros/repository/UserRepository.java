4d872b9e38a1b6fc2aa8844edef286e40aca0ea7
EmanuelleBrasil <emanuelle.rbl@gmail.com>
2021-04-07 09:56:44 -0300

    Repository User e LogErros

--- src/main/java/br/com/central_de_erros/repository/UserRepository.java
+++ src/main/java/br/com/central_de_erros/repository/UserRepository.java
@@ -1,4 +1,12 @@
 package br.com.central_de_erros.repository;
 
-public interface UserRepository {
+import br.com.central_de_erros.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
 }
