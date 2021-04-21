b1c912a95e4e2a9dfa6f79d19de49c18cfa7b23b
julioKauer <juliokauer@gmail.com>
2021-04-21 10:33:32 -0300

    Problema cors

--- src/main/java/br/com/central_de_erros/CentralDeErrosApplication.java
+++ src/main/java/br/com/central_de_erros/CentralDeErrosApplication.java
@@ -2,12 +2,31 @@ package br.com.central_de_erros;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
 
 @SpringBootApplication
 public class CentralDeErrosApplication {
+
+	private static final String ALL = "*";
 
 	public static void main(String[] args) {
 		SpringApplication.run(CentralDeErrosApplication.class, args);
 	}
 
+//	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry
+						.addMapping("/**")
+						.allowedOrigins(ALL)
+						.allowedHeaders(ALL)
+						.allowedMethods(ALL);
+			}
+		};
+	}
 }
